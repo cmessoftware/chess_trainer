eb04754b21c954a1a8c5c239dd0b9358917cde40
Sergio Salanitri <cmessoftware@gmail.com>
2026-06-03 21:11:17 -0300

    chore: clean old scripts

---
+++ backups/chess_trainer_db_backup_20260603_195540.sql
@@ -0,0 +1,26 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict L4mJFhUWJTmhJr4N0FAuOfEU2lD7QZ1A6qVYhRkUyhHZpsV7wiPLUxXDj3XUdR9
+
+-- Dumped from database version 13.22 (Debian 13.22-1.pgdg13+1)
+-- Dumped by pg_dump version 13.22 (Debian 13.22-1.pgdg13+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict L4mJFhUWJTmhJr4N0FAuOfEU2lD7QZ1A6qVYhRkUyhHZpsV7wiPLUxXDj3XUdR9
+
